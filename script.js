63af9adc8804c09eb423c8e132a98aa94fae1fe7
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-05-01 19:53:06 -0600

    working on functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var APIKey 6baaab2eaf88f88a13344b8b2da0190e
+let APIKey = "6baaab2eaf88f88a13344b8b2da0190e";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var city = $("#city").val();
 var searchBtn = $("#searchBtn");
@@ -6,3 +6,31 @@ var searchHistory = $("#searchHistory");
 var currentWeather = $("#currentWeather");
 var forecast = $("#forecast");
 var cityList = [];
+var localCityList = JSON.parse(localStorage.getItem("cityList")) || [];
+var localStorage = JSON.parse(localStorage.getItem("cityList")) || [];
+
+
+
+function displayCurrentWeather() {
+}
+
+function displayForecast() {
+}
+
+
+function displaySearchHistory() {
+}
+
+
+function displayUVIndex() {
+}
+
+function displayWeather() {
+    displayCurrentWeather();
+    displayForecast();
+    displayUVIndex();
+}
+
+function localStorage() {
+    localStorage.setItem("cityList", JSON.stringify(cityList));
+}
